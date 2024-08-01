5adbb942e2840d393044d9e5c69f2742150898a9
Ante Juric <antetg77@gmail.com>
2024-08-01 18:41:26 +0200

    API key change

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "46b7b7eee3634fcbbd414b79d360c975",
+    key: "478ff540d32a47b982482d262352e441",
   },
 });
